7fa647fdf9f9d55e20bb55f8f4cf12e1e7c0eae4
Szőcs Gábor <szocs@medgit.hu>
2025-03-04 17:45:14 +0100

    Add project files.

---
+++ WpfApp25/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp25
+{
+    internal class Person
+    {
+
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnNameChanged();
+            }
+        }
+
+
+        public event EventHandler NameChanged;
+        private void OnNameChanged()
+        {
+            if (NameChanged != null)
+            {
+                NameChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
